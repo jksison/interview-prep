9573c8f48fcf038c3292eb90cf2ddd8cff66d26a
jksison <johnk.sison@gmail.com>
2018-08-08 23:07:41 -0400

    added four characters test case for string permutation problem

--- javarevisited/Strings/src/test/java/StringPermutations/StringPermutationsTest.java
+++ javarevisited/Strings/src/test/java/StringPermutations/StringPermutationsTest.java
@@ -27,5 +27,41 @@ public class StringPermutationsTest {
 		assertEquals(expected, result);
 
 	}
+	
+	@Test
+	public void Permutate_FourCharacters_SixPermutations() {
+		Set<String> expected = new HashSet<String>();
+		
+		expected.add("acbd");
+		expected.add("adbc");
+		expected.add("bcad");
+		expected.add("bdac");
+		expected.add("cbad");
+		expected.add("cdab");
+		expected.add("abdc");
+		expected.add("acdb");
+		expected.add("bacd");
+		expected.add("bdca");
+		expected.add("cabd");
+		expected.add("cdba");
+		expected.add("dbac");
+		expected.add("dcab");
+		expected.add("abcd");
+		expected.add("adcb");
+		expected.add("badc");
+		expected.add("bcda");
+		expected.add("dabc");
+		expected.add("dcba");
+		expected.add("cadb");
+		expected.add("cbda");
+		expected.add("dacb");
+		expected.add("dbca");
+		
+		Set<String> result = StringPermutations.Solution.permutate("abcd");
+		
+		
+		assertEquals(expected, result);
+
+	}
 
 }
